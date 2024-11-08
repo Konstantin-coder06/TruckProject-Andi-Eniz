376a570565b25d30c52452e640a6a0b5919e23b1
Konstantin Mitkov <kocevelikibg06@gmail.com>
2024-11-08 11:33:04 +0200

    Entering database

---
+++ Documents/Software engineering/TruckData/Truck.Data/Data/Entities/TruckCompany.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TruckData.Data.Entities
+{
+    public class TruckCompany
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string TruckCompanyName { get; set; }
+        [Required]
+        public string TruckCompanyEmail { get; set; }
+        [Required]
+        public string TruckCompanyPassword { get; set; }
+        [Required]
+        public string MainTruckAdress { get; set; }
+        [Required]
+        public string TruckCompanyNumber { get; set; }
+
+    }
+}
---
+++ Documents/Software engineering/TruckData/Truck.Data/Data/Entities/TruckCompany_Shipment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TruckData.Data.Entities
+{
+    public class TruckCompany_Shipment
+    {
+        [ForeignKey("TruckCompany")]
+        public int TruckCompany_Id { get; set; }
+        public TruckCompany? TruckCompany { get; set; }
+        [ForeignKey("Shipment")]
+        public int Shipment_Id { get; set; }
+        public Shipment? Shipment { get; set; }
+    }
+}
