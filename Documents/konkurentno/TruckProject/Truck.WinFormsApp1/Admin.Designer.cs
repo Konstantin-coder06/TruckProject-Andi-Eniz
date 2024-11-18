5e8d09ab3a01786485d48644377aa1005f3c23e8
Konstantin-coder06 <kocevelikibg06@gmail.com>
2024-11-18 13:47:34 +0200

    Add Design

---
+++ Documents/konkurentno/TruckProject/Truck.WinFormsApp1/Admin.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Truck.WinFormsApp1
+{
+    partial class Admin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Admin));
+            label3 = new Label();
+            label2 = new Label();
+            richTextBox1 = new RichTextBox();
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            radioButton3 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            label1 = new Label();
+            pictureBox2 = new PictureBox();
+            button1 = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Black", 16F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label3.ForeColor = Color.Black;
+            label3.Location = new Point(479, 192);
+            label3.Name = "label3";
+            label3.Size = new Size(602, 37);
+            label3.TabIndex = 7;
+            label3.Text = "Choose category from where you will delete ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Black", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label2.ForeColor = Color.DarkCyan;
+            label2.Location = new Point(427, 105);
+            label2.Name = "label2";
+            label2.Size = new Size(398, 41);
+            label2.TabIndex = 6;
+            label2.Text = "Welcome back my Admin!";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.BackColor = SystemColors.ButtonFace;
+            richTextBox1.Location = new Point(479, 295);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(692, 630);
+            richTextBox1.TabIndex = 5;
+            richTextBox1.Text = "";
+            richTextBox1.TextChanged += richTextBox1_TextChanged;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.DarkCyan;
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(radioButton3);
+            panel1.Controls.Add(radioButton2);
+            panel1.Controls.Add(radioButton1);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(3, 1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(339, 1152);
+            panel1.TabIndex = 4;
+            panel1.Paint += panel1_Paint;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(33, 31);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(276, 173);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            radioButton3.ForeColor = SystemColors.ButtonFace;
+            radioButton3.Location = new Point(92, 527);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(244, 35);
+            radioButton3.TabIndex = 3;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "Receiving company";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            radioButton2.ForeColor = SystemColors.ButtonFace;
+            radioButton2.Location = new Point(92, 468);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(213, 35);
+            radioButton2.TabIndex = 2;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Selling company";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            radioButton1.ForeColor = SystemColors.ButtonFace;
+            radioButton1.Location = new Point(92, 417);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(111, 35);
+            radioButton1.TabIndex = 1;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Drivers";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Black", 18F, FontStyle.Bold | FontStyle.Italic | FontStyle.Underline, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ButtonFace;
+            label1.Location = new Point(12, 280);
+            label1.Name = "label1";
+            label1.Size = new Size(173, 41);
+            label1.TabIndex = 0;
+            label1.Text = "Categories";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(1278, 331);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(612, 408);
+            pictureBox2.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox2.TabIndex = 8;
+            pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            button1.Location = new Point(56, 354);
+            button1.Name = "button1";
+            button1.Size = new Size(120, 42);
+            button1.TabIndex = 5;
+            button1.Text = "Delete";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Admin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1902, 1153);
+            Controls.Add(pictureBox2);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(richTextBox1);
+            Controls.Add(panel1);
+            Name = "Admin";
+            Text = "Admin";
+            Load += Admin_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label3;
+        private Label label2;
+        private RichTextBox richTextBox1;
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Label label1;
+        private PictureBox pictureBox2;
+        private Button button1;
+    }
+}
